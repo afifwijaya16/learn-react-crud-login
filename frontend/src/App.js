5a3c7ab3a5f6663c370b78021a83e9cf62ebb9c1
afifwijaya16 <afif0071997@gmail.com>
2022-08-16 14:39:13 +0700

    user form add and edit page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./components/Login";
 import Users from "./pages/Users";
+import AddUser from "./pages/AddUser";
+import EditUser from "./pages/EditUser";
 import Products from "./pages/Products";
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/users" element={<Users />} />
+          <Route path="/users/add" element={<AddUser />} />
+          <Route path="/users/edit/:id" element={<EditUser />} />
           <Route path="/products" element={<Products />} />
         </Routes>
       </BrowserRouter>
