84c4b11e090f65618c2515291944f77627cffc84
afifwijaya16 <afif0071997@gmail.com>
2022-08-18 11:15:33 +0700

    product crud

--- frontend/src/components/FormAddProduct.js
+++ frontend/src/components/FormAddProduct.js
@@ -1,23 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const FormAddProduct = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [msg, setMsg] = useState("");
+
+  const navigate = useNavigate();
+
+  const saveProduct = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/products", {
+        name: name,
+        price: price,
+      });
+      navigate("/products");
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.data.msg);
+      }
+    }
+  };
   return (
     <div>
       <h1 className="title">Product</h1>
       <h2 className="subtitle">Add New Product</h2>
       <div className="card p-2 mr-2 is-shadowless">
         <div className="content">
-          <form>
+          <form onSubmit={saveProduct}>
+            <p className="has-text-centered">{msg}</p>
             <div className="field">
               <label className="label">Name</label>
               <div className="control">
-                <input type="text" className="input" placeholder="Name" />
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
               </div>
             </div>
             <div className="field">
               <label className="label">Price</label>
               <div className="control">
-                <input type="number" className="input" placeholder="Price" />
+                <input
+                  type="number"
+                  className="input"
+                  placeholder="Price"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
               </div>
             </div>
             <div className="field">
--- frontend/src/components/Productlist.js
+++ frontend/src/components/Productlist.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Productlist = () => {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const response = await axios.get("http://localhost:5000/products");
+    setProducts(response.data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const deleteProduct = async (productId) => {
+    await axios.delete(`http://localhost:5000/products/${productId}`);
+    getProducts();
+  };
+
   return (
     <div>
       <h1 className="title">Products</h1>
       <h2 className="subtitle">List of Products</h2>
+      <Link to="/products/add" className="button is-small is-primary mb-2">
+        Add New
+      </Link>
       <table className="table is-striped is-fullwidth">
         <thead>
           <tr>
@@ -16,13 +37,28 @@ const Productlist = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
+          {products.map((i, index) => (
+            <tr key={i.uuid}>
+              <td>{index + 1}</td>
+              <td>{i.name}</td>
+              <td>{i.price}</td>
+              <td>{i.user.name}</td>
+              <td>
+                <Link
+                  to={`/products/edit/${i.uuid}`}
+                  className="button is-small is-info mr-1"
+                >
+                  Edit
+                </Link>
+                <button
+                  onClick={() => deleteProduct(i.uuid)}
+                  className="button is-small is-danger"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
